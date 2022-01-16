c6ecae52db75d3539dacc1331d8262f4f0beb839
TakeHere <73341981+TakeHere@users.noreply.github.com>
2022-01-16 21:51:21 +0100

    Add files via upload

--- src/main/java/fr/takehere/minecraftvids/minecraftvids/MinecraftVids.java
+++ src/main/java/fr/takehere/minecraftvids/minecraftvids/MinecraftVids.java
@@ -1,86 +1,85 @@
 package fr.takehere.minecraftvids.minecraftvids;
 
 import com.destroystokyo.paper.ParticleBuilder;
-import it.unimi.dsi.fastutil.Hash;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.DemuxerTrack;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public final class MinecraftVids extends JavaPlugin {
 
-    int currentImage = 0;
+    Location spawnLocation;
+    File video = new File(this.getDataFolder().getAbsolutePath() + File.separator + "video.mp4");
+    int totalFrames = 0;
+    List<Picture> pictures = new ArrayList<>();
 
     @Override
     public void onEnable() {
-        Location spawnLocation = new Location(Bukkit.getWorld("world"),0,400, 0);
-        List<Image> images = new ArrayList<>();
+        spawnLocation = new Location(Bukkit.getWorld("world"),0,400, 0);
 
-        new Thread(() -> {
+        Thread t = new Thread(() -> {
             try {
-                File dir = new File(this.getDataFolder().getAbsolutePath());
-                File[] files = dir.listFiles();
+                FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(video));
+                DemuxerTrack vt = grab.getVideoTrack();
+                totalFrames = vt.getMeta().getTotalFrames();
 
-                for (int i = 0; i < files.length; i++) {
-                    System.out.println(i + "/" + files.length);
-
-                    File file = files[i];
-
-                    BufferedImage bi = ImageIO.read(file);
-                    Image image = new Image();
-
-                    for (int x = 0; x < bi.getWidth(); x++) {
-                        for (int y = 0; y < bi.getHeight(); y++) {
-                            int clr = bi.getRGB(x, y);
-                            int red =   (clr & 0x00ff0000) >> 16;
-                            int green = (clr & 0x0000ff00) >> 8;
-                            int blue =   clr & 0x000000ff;
-
-                            Color pixelColor = Color.fromRGB(red, green, blue);
-                            image.pixels.put(new Vector2(x, y), pixelColor);
-                        }
-                    }
-
-                    images.add(image);
+                for (int i = 0; i < totalFrames; i++) {
+                    Picture picture = FrameGrab.getFrameFromFile(video, i);
+                    pictures.add(picture);
+                    System.out.println(i + "/" + totalFrames);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
+            } catch (JCodecException e) {
+                e.printStackTrace();
             }
-        }).run();
+            System.out.println("Frames grabbing ended, start rendering");
+            render();
+        });
+        t.start();
+    }
 
-        ParticleBuilder particleBuilder = new ParticleBuilder(Particle.REDSTONE);
+    ParticleBuilder particleBuilder = new ParticleBuilder(Particle.REDSTONE);
+    int currentImage = 0;
 
+    public void render(){
         Bukkit.getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
-                Image image = images.get(currentImage);
-                for (Map.Entry<Vector2, Color> vector2ColorEntry : image.pixels.entrySet()) {
-                    Color color = vector2ColorEntry.getValue();
-                    Vector2 vector2 = vector2ColorEntry.getKey();
-                    Location newLocation = new Location(spawnLocation.getWorld(), spawnLocation.getX() + (vector2.x), spawnLocation.getY() - vector2.y, spawnLocation.getZ());
-
-                    particleBuilder.color(color, 10f).location(newLocation).spawn();
+                BufferedImage bi = AWTUtil.toBufferedImage(pictures.get(currentImage));
+                for (int x = 0; x < bi.getWidth(); x++) {
+                    for (int y = 0; y < bi.getHeight(); y++) {
+                        Location newLocation = new Location(spawnLocation.getWorld(), spawnLocation.getX() + x, spawnLocation.getY() - y, spawnLocation.getZ());
+                        particleBuilder.color(getColorAt(bi, x, y), 10f).location(newLocation).spawn();
+                    }
                 }
 
                 currentImage++;
             }
-        },0,5);
+        },0,3);
     }
 
-    @Override
-    public void onDisable() {
+    public Color getColorAt(BufferedImage image, int x, int y){
+        int clr = image.getRGB(x, y);
+        int red =   (clr & 0x00ff0000) >> 16;
+        int green = (clr & 0x0000ff00) >> 8;
+        int blue =   clr & 0x000000ff;
 
+        return Color.fromRGB(red, green, blue);
     }
 }
